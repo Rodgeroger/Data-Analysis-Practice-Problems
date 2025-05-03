5a2f69d0f8d7f9bc5835440d6ad41bdb5604a4ef
Rodrigo Isidro <182361412+Rodgeroger@users.noreply.github.com>
2025-05-03 02:19:45 -0700

    Add files via upload

--- Customer Sales Analysis/Sales_data_Analysis.sql
+++ Customer Sales Analysis/Sales_data_Analysis.sql
@@ -2,60 +2,56 @@
 SELECT * 
 FROM sales_data;
 
-
 /*Calculate total sales for each category order by most sales*/
 SELECT 
+	product AS Product,
 	category AS Category,
-    	ROUND(SUM(quantity * unit_price), 2) AS total_sales
+	SUM(unit_price) as 'Total Sales'
 FROM sales_data
 GROUP BY product, category
-ORDER by total_sales DESC;
-
+ORDER by 'Total Sales' DESC;
 
 /*Find the top 5 customers by total spending*/
 SELECT TOP 5
 	customer_id,
-    	ROUND(SUM(quantity * unit_price), 2) AS total_spent
+	sum(unit_price) as 'Total Spending'
 FROM sales_data
 GROUP BY customer_id
-ORDER BY total_spent DESC;
-
+ORDER BY 'Total Spending' DESC;
 
 /*Determine which region had the highest average order value.*/
 --cte to find amount of orders per region
 WITH Amount_of_Orders_per_region AS
 	(SELECT
-		COUNT(*) AS Amount_of_Orders,
+		COUNT(*) AS 'Amount of Orders',
 		region
 	FROM sales_data
 	GROUP BY region)
 SELECT TOP 1
 	sd.region,
-    	ROUND(AVG(quantity * unit_price), 2) AS avg_order_value
-	ar.Amount_of_Orders
+	ROUND(AVG(unit_price),2) AS 'Average Order Value',
+	ar.[Amount of Orders]
 FROM sales_data AS sd
 JOIN Amount_of_Orders_per_region AS ar
 ON sd.region = ar.region
-GROUP BY sd.region, ar.Amount_of_Orders
-ORDER BY avg_order_value DESC;
-
+GROUP BY sd.region, ar.[Amount of Orders]
+ORDER BY 'Average Order Value' DESC;
 
 /*Identify the month with the highest sales overall.*/
 --cte to find amount of orders per month
-/* Identify the month with the highest sales overall using a CTE */
 WITH Amount_of_Sales_By_Month AS 
 	(SELECT
-		FORMAT(order_date, 'yyyy-MM') AS order_month,
-		COUNT(*) AS Amount_of_Orders
+		COUNT(*) AS 'Amount of Orders',
+		MONTH(order_date) AS Month
 	FROM sales_data
-	GROUP BY FORMAT(order_date, 'yyyy-MM'))
+	GROUP BY MONTH(order_date)
+	)
 SELECT TOP 1
-	sd_month.order_month,
-	ROUND(SUM(sd.quantity * sd.unit_price), 2) AS total_sales,
-	am.Amount_of_Orders
-FROM sales_data sd
-JOIN Amount_of_Sales_By_Month am
-ON FORMAT(sd.order_date, 'yyyy-MM') = am.order_month
-GROUP BY sd_month.order_month, am.Amount_of_Orders
-ORDER BY total_sales DESC;
-
+	MONTH(sd.order_date) AS Month,
+	SUM(sd.unit_price) AS 'Overall Sales',
+	am.[Amount of Orders]
+FROM sales_data AS sd
+JOIN Amount_of_Sales_By_Month AS am
+ON MONTH(sd.order_date) = am.month
+GROUP BY MONTH(sd.order_date), am.[Amount of Orders]
+ORDER BY 'Overall Sales' DESC
